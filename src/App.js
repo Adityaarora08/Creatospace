8dcd5b598fe1ab963bfcc45bca1ff50a7fbf606f
Adityaarora08 <79107244+Adityaarora08@users.noreply.github.com>
2022-08-24 06:43:39 -0700

    Creatospace

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://creatospace.com/assets/image/logofull.png" className="App-logo" alt="logo" />
+        <p>
+          Register for FREE to meet your favourite creators in their Metaverse
+        </p>
+        <button id="joinButton">Join the Waiting List</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
